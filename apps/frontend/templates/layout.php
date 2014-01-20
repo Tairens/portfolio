ed429a4b2c7b26d4623a5a366d24f4c775499068
Tairens <tairens@gmail.com>
2014-01-20 10:41:08 +0100

    Pierwszy commit.

---
+++ apps/frontend/templates/layout.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+  <head>
+    <?php include_http_metas() ?>
+    <?php include_metas() ?>
+    <?php include_title() ?>
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <?php include_stylesheets() ?>
+    <?php include_javascripts() ?>
+  </head>
+  <body>
+    <script src="http://code.jquery.com/jquery.js"></script>
+	<div class="container">
+		<div class="row">
+			<div class="header">
+			
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-3">
+				<div class="buttons">
+					<div class="row">
+						<div class="col-md-2"></div>
+						<div class="col-md-10">
+							<div class="mainButton"><h4>Buahahahhahaha</h4></div>
+						</div>
+					</div>
+					</br>
+					<div class="row">
+						<div class="col-md-2"></div>
+						<div class="col-md-10">
+							<div class="mainButton"><h4>Buahahahhahaha</h4></div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="col-md-12">
+					<div class="mainContent">
+					
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="buttons">
+					<div class="row">
+						<div class="col-md-10">
+							<div class="mainButton"><h4>Buahahahhahaha</h4></div>
+						</div>
+					</div>
+					</br>
+					<div class="row">
+						<div class="col-md-10">
+							<div class="mainButton"><h4>Buahahahhahaha</h4></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		</br>
+		<div class="row">
+			<div id="carousel-example-generic2" class="carousel slide">
+			  <!-- Wskaźniki w postaci kropek -->
+			  
+			 
+			  <!-- Slajdy -->
+			  <div class="carousel-inner">
+				<div class="item active">
+				  <img src="http://placehold.it/1280x100" alt="">
+				  <!-- Opis slajdu -->
+				  <div class="carousel-caption">
+					<h3>To jest opis</h3>
+					<p>pierwszego slajdu</p>
+				  </div>
+				</div>
+			 
+				<div class="item">
+				  <img src="http://placehold.it/1280x100" alt="">
+				  <!-- Opis slajdu -->
+				  <div class="carousel-caption">
+					<h3>To jest opis</h3>
+					<p>drugiego slajdu</p>
+				  </div>
+				</div>
+			 
+				<div class="item">
+				  <img src="http://placehold.it/1280x100" alt="">
+				  <!-- Opis slajdu -->
+				  <div class="carousel-caption">
+					<h3>To jest opis</h3>
+					<p>trzeciego slajdu</p>
+				  </div>
+				</div>
+			 
+			  </div>
+			 <ol class="carousel-indicators">
+				<li data-target="#carousel-example-generic2" data-slide-to="0" class="active"></li>
+				<li data-target="#carousel-example-generic2" data-slide-to="1"></li>
+				<li data-target="#carousel-example-generic2" data-slide-to="2"></li>
+			  </ol>
+			  <!-- Strzałki do przewijania -->
+			  <a class="left carousel-control" href="#carousel-example-generic2" data-slide="prev">
+				<span class="icon-prev"></span>
+			  </a>
+			  <a class="right carousel-control" href="#carousel-example-generic2" data-slide="next">
+				<span class="icon-next"></span>
+			  </a>
+			</div>
+		</div>
+	</div>
+  </body>
+</html>
